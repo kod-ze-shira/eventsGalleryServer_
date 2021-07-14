5e7693a5a8f73e68cfee8fc492d312469f4c571f
GilaWizel <gila@leader.codes>
2021-07-14 12:32:31 +0000

    tamarr:

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    APi_URL_CONTACT:'https://api.leader.codes',
-    API_URL_MAIL:'https://mails.codes/mail/sendEmail',
-    API_URL_ACCOUNT:'https://accounts.codes',
-    JWT:'jwt'
-}
